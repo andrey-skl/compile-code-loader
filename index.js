0cfebd59e5df5a29aed4f548f01bbcb74bee7de2
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-15 15:09:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
+
+module.exports = function () {};
+
+module.exports.pitch = function (request) {
+    if (!this.webpack) throw new Error("Only usable with webpack");
+
+    var callback = this.async();
+
+    var compiler = this._compilation.createChildCompiler("compile-loader", {});
+    compiler.apply(new SingleEntryPlugin(this.context, "!!" + request, "main"));
+
+    compiler.runAsChild(function (err, entries, compilation) {
+        if(err) return callback(err);
+        if (entries[0]) {
+            var compiledFile = entries[0].files[0];
+            return callback(null, compilation.assets[compiledFile].source());
+        } else {
+            return callback(null, null);
+        }
+    });
+};
