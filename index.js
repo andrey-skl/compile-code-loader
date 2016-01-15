a5b85d167ec22bc5e417e68bf194832edd85ed24
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-01-15 15:33:43 +0300

    Pass query to compiler

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var path = require("path");
 var SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
+var loaderUtils = require("loader-utils");
 
 module.exports = function () {};
 
 module.exports.pitch = function (request) {
     if (!this.webpack) throw new Error("Only usable with webpack");
 
+    var query = loaderUtils.parseQuery(this.query) || {};
+
     var callback = this.async();
     this.cacheable();
 
-    var compiler = this._compilation.createChildCompiler("compile-loader", {});
+    var compiler = this._compilation.createChildCompiler("compile-loader", query);
     compiler.apply(new SingleEntryPlugin(this.context, "!!" + request, "main"));
 
     compiler.runAsChild(function (err, entries, compilation) {
